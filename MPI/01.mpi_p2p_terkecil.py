a09ecf244a86d23599600ee889d781052a2c4020
Naufal Ibadurrahman <naufal455@gmail.com>
2020-04-14 22:25:23 +0700

    Update 01.mpi_p2p_terkecil.py
    menambah fitur : 
    1. rank 0 mengirim pesan
    2. rank sisa menerima pesan

--- MPI/01.mpi_p2p_terkecil.py
+++ MPI/01.mpi_p2p_terkecil.py
@@ -9,3 +9,18 @@ rank = comm.rank
 
 # dapatkan total proses berjalan
 size = comm.size
+
+# jika saya rank ke 0 maka saya akan mengirimkan pesan ke proses yang mempunyai rank 1 s.d size
+pesan = 999
+if rank == 0:
+    print("rank",rank, "Mengirim")
+    i=1
+    while i < (size):
+        comm.send(pesan, dest=i)
+        i=i+1
+	
+
+# jika saya bukan rank 0 maka saya menerima pesan yang berasal dari proses dengan rank 0
+else:
+    terima = comm.recv(source=0) 
+    print("rank",rank,"dapat pesan", terima)
