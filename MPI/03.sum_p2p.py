5409558dae79c76c96eed3b6640a5252b7facbae
Naufal Ibadurrahman <naufal455@gmail.com>
2020-04-14 22:36:27 +0700

    Update 03.sum_p2p.py
    Menambah variable : 
    1. Hasil, untuk penampung total value dari semua rank
    
    Menambah fungsi : 
    1. jika rank != 0, akan mengirim random number ke rank 0
    2. jika rank ==0, menerima semua number dari sumber rank, lalu melakukan kalkulasi

--- MPI/03.sum_p2p.py
+++ MPI/03.sum_p2p.py
@@ -16,3 +16,21 @@ size = comm.size
 # generate angka integer secara random untuk setiap proses
 angka = random.randint(1, size)
 print("Rank",rank,"punya angka", angka)
+
+hasil = 0
+
+# jika saya adalah proses dengan rank 0 maka:
+# saya menerima nilai dari proses 1 s.d proses dengan rank terbesar
+# menjumlah semua nilai yang didapat (termasuk nilai proses saya)
+if rank == 0 :
+    i=1
+    hasil = hasil + angka
+    while i < (size):
+        dapet = comm.recv(source=i)
+        i=i+1
+        hasil = hasil + dapet
+    print("Rank", rank, "Menjumlahkan jadi : ", hasil)
+	
+# jika bukan proses dengan rank 0, saya akan mengirimkan nilai proses saya ke proses dengan rank=0
+else:
+    kirim = comm.send(angka, dest=0)
