a99f7a9773fc88813604da1587e03e761da42a90
Naufal Ibadurrahman <naufal455@gmail.com>
2020-04-14 22:58:00 +0700

    Update 05.mpi_pi.py
    menambah fungsi pi dan melakukan deklarasi variabel

--- MPI/05.mpi_pi.py
+++ MPI/05.mpi_pi.py
@@ -14,3 +14,39 @@ def local_loop(num_steps,begin,end):
         sum = sum + 4.0/(1.0+x*x)
     #print(sum)
     return sum*step 
+
+# fungsi Pi
+def Pi():
+    
+    # buat COMM
+    comm = MPI.COMM_WORLD
+    
+    # dapatkan rank proses
+    rank = comm.rank
+    
+    # dapatkan total proses berjalan
+    size = comm.size
+    
+    # lakukan penjumlahan dari local_sum proses-proses yang ada ke proses 0
+    # bisa digunakan reduce atau p2p sum
+    if rank == 0:
+        n = size
+    else :
+        n = None
+    
+    n = comm.bcast(n, root=0)
+
+    # value dari local_loop
+    z = local_loop(n, rank, size)
+
+    phi = comm.reduce(z , op=MPI.SUM, root=0)
+
+    # jika saya proses dengan rank 0  maka tampilkan hasilnya
+    if rank == 0:
+        selisih = phi-math.pi
+        print ("Nilai phi terdekat : ", phi, "Dengan Selisih : ", selisih)
+        print ("Nilai Phi Real     : ", math.pi)
+    
+# panggil fungsi utama    
+if __name__ == '__main__':
+    Pi()
