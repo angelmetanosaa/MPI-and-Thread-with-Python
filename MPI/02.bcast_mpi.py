9daba5e1cfc8d9fc29b6ca6a8ce189a9e5571e43
Naufal Ibadurrahman <naufal455@gmail.com>
2020-04-14 22:28:36 +0700

    Update 02.bcast_mpi.py
    menambah fitur : 
    1.  jika rank 0, kirim pesan bcast
    2. selain rank 0, terima bcast

--- MPI/02.bcast_mpi.py
+++ MPI/02.bcast_mpi.py
@@ -9,3 +9,19 @@ rank = comm.rank
 
 # dapatkan total proses berjalan
 size = comm.size
+
+# jika saya rank 0 maka saya akan melakukan broadscast
+if rank == 0 :
+    pesan = 777
+else :
+    pesan = None
+
+# Lakukan Broadcasr pesan
+pesan = comm.bcast(pesan, root=0)
+    
+	
+# jika saya bukan rank 0 maka saya menerima pesan
+if rank == 0:
+    print('Proses {} broadcast data:'.format(rank), pesan)
+else :
+    print('Proses {} terima data:'.format(rank), pesan)
