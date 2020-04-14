4d34c82c20d2d981a6590c0c550ef5ac047fac39
Naufal Ibadurrahman <naufal455@gmail.com>
2020-04-14 22:18:21 +0700

    Update 01.mpi_p2p_terbesar.py
    Menambahkan fitur untuk melakukan pengiriman jika rank==max
    dan menerima jika rank lebih rendah

--- MPI/01.mpi_p2p_terbesar.py
+++ MPI/01.mpi_p2p_terbesar.py
@@ -9,3 +9,17 @@ rank = comm.rank
 
 # dapatkan total proses berjalan
 size = comm.size
+
+# jika saya rank terbesar maka saya akan mengirimkan pesan ke proses yang mempunyai rank 0 s.d rank terbesar-1
+pesan = 999
+if rank == size-1 :
+    print("rank",rank, "Mengirim")
+    i=0
+    while i < (size-1):
+        comm.send(pesan, dest=i)
+        i=i+1
+
+# jika saya bukan rank terbesar maka saya akan menerima pesan yang berasal dari proses dengan rank terbesar
+elif (rank<size-1):
+    terima = comm.recv(source=size-1) 
+    print("rank",rank,"dapat pesan", terima)
