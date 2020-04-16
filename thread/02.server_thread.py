bc3bdc8106390756cd493c8eca11d5d099fd2e66
Angel Metanosa Afinda <54460286+angelmetanosaa@users.noreply.github.com>
2020-04-16 18:04:08 +0700

    Thread V.4.2
    1. Melakukan infinite loop, jangan reset setiap ada request
    2. Melakukan pembuatan fungsi client_thread
    3. Melakukan pemanggilan fungsi utama

--- thread/02.server_thread.py
+++ thread/02.server_thread.py
@@ -39,57 +39,59 @@ def start_server():
     # infinite loop, jangan reset setiap ada request
     while True:
         # terima koneksi
+        c, addr = soc.accept()
         
-        
-        # dapatkan IP dan port
-        
-        print("Connected dengan " + ip + ":" + port)
-
+        # dapatkan IP dan port 
+        ip = addr[0]
+        port = addr[1]
+        print("Connected dengan " + ip + ":" + str(port))
         # jalankan thread untuk setiap koneksi yang terhubung
         try:
-            
+            t = client_thread(c,ip,port,4096)
+            if(t!=None):
+                t.start()
         except:
             # print kesalahan jika thread tidak berhasil dijalankan
             print("Thread tidak berjalan.")
             traceback.print_exc()
 
     # tutup socket
-    
+    soc.close()
 
 
-def client_thread(connection, ip, port, max_buffer_size = 4096):
+def client_thread (connection, ip, port, max_buffer_size = 4096):
     # flag koneksi
-    
+    is_active = True
 
     # selama koneksi aktif
     while is_active:
 
         # terima pesan dari client
-        
+        data = connection.recv(max_buffer_size)
         
         # dapatkan ukuran pesan
-        
+        client_input_size = sys.getsizeof(data)
         
         # print jika pesan terlalu besar
         if client_input_size > max_buffer_size:
             print("The input size is greater than expected {}")
 
         # dapatkan pesan setelah didecode
-        
+        client_input = data.decode()
         
         # jika "quit" maka flag koneksi = false, matikan koneksi
         if "quit" in client_input:
             # ubah flag
-        
+            is_active = False
             print("Client meminta keluar")
             
             # matikan koneksi
-            
-            print("Connection " + ip + ":" + port + " ditutup")
+            connection.close()
+            print("Connection " + ip + ":" + str(port) + " ditutup")
             
         else:
             # tampilkan pesan dari client
-            
+            print(data)
             
 # panggil fungsi utama
 if __name__ == "__main__":
